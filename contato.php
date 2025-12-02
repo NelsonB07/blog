4e9555e568ad00ed283abd3b4fdd52904665a69a
Nelson Brandão Carvalho. <jbrandao.72@gmail.com>
2025-12-02 02:14:03 -0300

    Desenvolvendo arquivos

--- contato.php
+++ contato.php
@@ -1,12 +1,9 @@
-
 <?php 
     include_once('templates/header.php');
-    ?>
-
-<h1>Olá, Mundo!</h1>
+?>
+  
+<h1>Página de contato</h1>
 
 <?php 
     include_once('templates/footer.php');
 ?>
-
-
---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>Nelson Brandão Carvalho &copy; 2025</p>
+</footer>
+</body>
+</html>
