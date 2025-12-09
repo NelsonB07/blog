276359112f207625af1f573a395e9388a0ebc485
Nelson Brandão Carvalho. <jbrandao.72@gmail.com>
2025-12-09 01:55:36 -0300

    Estilizando a página

--- templates/header.php
+++ templates/header.php
@@ -15,7 +15,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-</head>""
+</head>
 <body>
 <header>
     <a href="<?= $BASE_URL ?>" id="logo">
@@ -23,10 +23,10 @@
     </a>
     <nav>
         <ul id="navbar">
-            <li><a href="<?= $BASE_URL ?>">Home</a></li>
-            <li><a href="#">Categorias</a></li>
-            <li><a href="#">Sobre</a></li>
-            <li><a href="<?= $BASE_URL ?>contato.php">Contato</a></li>
+            <li><a href="<?= $BASE_URL ?>" class="nav-link">Home</a></li>
+            <li><a href="#" class="nav-link">Categorias</a></li>
+            <li><a href="#" class="nav-link">Sobre</a></li>
+            <li><a href="<?= $BASE_URL ?>contato.php" class="nav-link">Contato</a></li>
         </ul>
     </nav>
 </header>
