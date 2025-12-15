af0e817076188da07de9b330a3cbcf15676c9476
Nelson Brandão Carvalho. <jbrandao.72@gmail.com>
2025-12-15 01:12:29 -0300

    Estudos PHP

--- index.php
+++ index.php
@@ -3,7 +3,22 @@
     include_once('templates/header.php');
     ?>
 
-<h1>Olá, Mundo!</h1>
+<main>
+    <div id="title-container">
+        <h1>Blog Brandão</h1>
+        <p>O seu blog de programação</p>
+    </div>
+    <div id="posts-container">
+        <?php foreach($posts as $post): ?>
+            <div class="post-box">
+                <img src="<?= $BASE_URL ?>/img/<?= $post['img'] ?>" alt="<=? $post['title] ?>"
+                <h2 class="pos-title">
+                    <a href="<?= $BASE_URL ?>post.php/id=<?= $post['id'] ?>" ><?= $post['img'] ?>"></a>
+                </h2>
+            </div>  
+         <?php endforeach; ?>     
+    </div>            
+</main>
 
 <?php 
     include_once('templates/footer.php');
